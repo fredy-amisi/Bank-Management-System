f4684b2ef3e6d44170210a9b280df87e52fb9890
fredy-Amisi <amisifredy20@gmail.com>
2024-07-18 17:38:13 +0300

    update 1.2

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,23 +2,25 @@ import React from "react";
 
 export const Header = (props) => {
   return (
-    <header id="header">
+    <header id="header" style={{ height: "100vh", minHeight: "500px" }}>
       <div className="intro">
         <div className="overlay">
           <div className="container">
             <div className="row">
-              <div className="col-md-8 col-md-offset-2 intro-text">
-                <h1>
+              <div className="col-md-8 col-md-offset- intro-text">
+                <h1 style={{ fontSize: "2rem" }}>
                   {props.data ? props.data.title : "Loading"}
                   <span></span>
                 </h1>
                 <p>{props.data ? props.data.paragraph : "Loading"}</p>
-                <a
-                  href="#features"
-                  className="btn btn-custom btn-lg page-scroll"
-                >
-                  Learn More
-                </a>{" "}
+                <div className="button-container">
+                  <a href="#signup" className="btn btn-custom btn-lg page-scroll">
+                    SignUp
+                  </a>{" "}
+                  <a href="#login" className="btn btn-custom btn-lg page-scroll">
+                    Login
+                  </a>{" "}
+                </div>
               </div>
             </div>
           </div>
